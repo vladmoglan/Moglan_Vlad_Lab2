21abc0f03ebec3633fbdf18781c97fb2283acdb0
VLAD-ANDREI MOGLAN <vlad.moglan@stud.ubbcluj.ro>
2020-11-01 23:34:50 +0200

    Laborator 4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,15 +26,50 @@ namespace Moglan_Vlad_Lab2
         private int mFilledLemon;
         private int mFilledChocolate;
         private int mFilledVanilla;
+        DoughnutType selectedDoughnut;
         public MainWindow()
         {
             InitializeComponent();
+            //creare obiect binding pentru comanda
+            CommandBinding cmd1 = new CommandBinding();
+            //asociere comanda
+            cmd1.Command = ApplicationCommands.Print;
+            //input gesture: I + Alt
+            ApplicationCommands.Print.InputGestures.Add(
+            new KeyGesture(Key.I, ModifierKeys.Alt));
+            //asociem un handler
+            cmd1.Executed += new ExecutedRoutedEventHandler(CtrlP_CommandHandler);
+            //adaugam la colectia CommandBindings
+            this.CommandBindings.Add(cmd1);
+
+            //Doughnuts>Stop
+            //comanda custom
+            CommandBinding cmd2 = new CommandBinding();
+            cmd2.Command = CustomCommands.StopCommand.Launch;
+            cmd2.Executed += new
+            ExecutedRoutedEventHandler(CtrlS_CommandHandler);//asociem handler
+            this.CommandBindings.Add(cmd2);
+        }
+
+        private void CtrlP_CommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("You have in stock:" + mRaisedGlazed + " Glazed," + mRaisedSugar + "Sugar, " + mFilledLemon + " Lemon, " + mFilledChocolate + " Chocolate, " + mFilledVanilla + " Vanilla");
+        }
+
+        private void CtrlS_CommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            //handler pentru comanda Ctrl+S -> se va executa stopToolStripMenuItem_Click
+            MessageBox.Show("Ctrl+S was pressed! The doughnut machine will stop!");
+            this.stopToolStripMenuItem_Click(sender, e);
         }
 
         private void frmMain_Loaded(object sender, RoutedEventArgs e)
         {
             myDoughnutMachine = new DoughnutMachine();
             myDoughnutMachine.DoughnutComplete += new DoughnutMachine.DoughnutCompleteDelegate(DoughnutCompleteHandler);
+            cmbType.ItemsSource = PriceList;
+            cmbType.DisplayMemberPath = "Key";
+            cmbType.SelectedValuePath = "Value";
         }
 
         private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs e)
@@ -105,6 +140,130 @@ namespace Moglan_Vlad_Lab2
                     break;
             }
         }
+
+        private void FilledItems_Click(object sender, RoutedEventArgs e)
+        {
+            string DoughnutFlavour;
+
+            MenuItem SelectedItem = (MenuItem)e.OriginalSource;
+            DoughnutFlavour = SelectedItem.Header.ToString();
+            Enum.TryParse(DoughnutFlavour, out DoughnutType myFlavour);
+            myDoughnutMachine.MakeDoughnuts(myFlavour);
+
+        }
+
+        private void FilledItemsShow_Click(object sender, RoutedEventArgs e)
+        {
+            string mesaj;
+            MenuItem SelectedItem = (MenuItem)e.OriginalSource;
+            mesaj = SelectedItem.Header.ToString() + " doughnuts are being cooked!";
+            this.Title = mesaj;
+        }
+
+        private void cmbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtPrice.Text = cmbType.SelectedValue.ToString();
+            KeyValuePair<DoughnutType, double> selectedEntry =
+           (KeyValuePair<DoughnutType, double>)cmbType.SelectedItem;
+            selectedDoughnut = selectedEntry.Key;
+        }
+
+        private int ValidateQuantity(DoughnutType selectedDoughnut)
+        {
+            int q = int.Parse(txtQuantity.Text);
+            int r = 1;
+
+            switch (selectedDoughnut)
+            {
+                case DoughnutType.Glazed:
+                    if (q > mRaisedGlazed)
+                        r = 0;
+                    break;
+                case DoughnutType.Sugar:
+                    if (q > mRaisedSugar)
+                        r = 0;
+                    break;
+                case DoughnutType.Chocolate:
+                    if (q > mFilledChocolate)
+                        r = 0;
+                    break;
+                case DoughnutType.Lemon:
+                    if (q > mFilledLemon)
+                        r = 0;
+                    break;
+                case DoughnutType.Vanilla:
+                    if (q > mFilledVanilla)
+                        r = 0;
+                    break;
+            }
+            return r;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateQuantity(selectedDoughnut) > 0)
+            {
+                lstSale.Items.Add(txtQuantity.Text + " " + selectedDoughnut.ToString() +
+               ":" + txtPrice.Text + " " + double.Parse(txtQuantity.Text) *
+               double.Parse(txtPrice.Text));
+            }
+            else
+            {
+                MessageBox.Show("Cantitatea introdusa nu este disponibila in stoc!");
+            }
+        }
+
+        private void btnRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            lstSale.Items.Remove(lstSale.SelectedItem);
+        }
+
+        private void btnCheckOut_Click(object sender, RoutedEventArgs e)
+        {
+            txtTotal1.Text = (double.Parse(txtTotal1.Text) + double.Parse(txtQuantity.Text)
+           * double.Parse(txtPrice.Text)).ToString();
+            foreach (string s in lstSale.Items)
+            {
+                switch (s.Substring(s.IndexOf(" ") + 1, s.IndexOf(":") - s.IndexOf(" ") -
+               1))
+                {
+                    case "Glazed":
+                        mRaisedGlazed = mRaisedGlazed - Int32.Parse(s.Substring(0,
+                       s.IndexOf(" ")));
+                        txtGlazedRaised.Text = mRaisedGlazed.ToString();
+                        break;
+                    case "Sugar":
+                        mRaisedSugar = mRaisedSugar - Int32.Parse(s.Substring(0,
+                       s.IndexOf(" ")));
+                        txtSugarRaised.Text = mRaisedSugar.ToString();
+                        break;
+                    case "Chocolate":
+                        mFilledChocolate = mFilledChocolate - Int32.Parse(s.Substring(0,
+                       s.IndexOf(" ")));
+                        txtChocolateFilled.Text = mFilledChocolate.ToString();
+                        break;
+                    case "Lemon":
+                        mFilledLemon = mFilledLemon - Int32.Parse(s.Substring(0,
+                       s.IndexOf(" ")));
+                        txtLemonFilled.Text = mFilledLemon.ToString();
+                        break;
+                    case "Vanilla":
+                        mFilledVanilla = mFilledVanilla - Int32.Parse(s.Substring(0,
+                       s.IndexOf(" ")));
+                        txtVanillaFilled.Text = mFilledVanilla.ToString();
+                        break;
+                }
+            }
+        }
+
+        KeyValuePair<DoughnutType, double>[] PriceList = {
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Sugar, 2.5),
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Glazed,3),
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Chocolate,4.5),
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Vanilla,4),
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Lemon,3.5)
+        };
+
         private void stopToolStripMenuItem_Click(object sender, RoutedEventArgs e)
         {
             myDoughnutMachine.Enabled = false;
@@ -121,9 +280,11 @@ namespace Moglan_Vlad_Lab2
         {
             if (!(e.Key >= Key.D0 && e.Key <= Key.D9))
             {
-                MessageBox.Show("Se pot introduce numai numere!", "Input Error", MessageBoxButton.OK,
+                MessageBox.Show("Se pot introduce numai cifre!", "Input Error", MessageBoxButton.OK,
                MessageBoxImage.Error);
             }
         }
+
+
     }
 }
---
+++ StopCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Moglan_Vlad_Lab2.CustomCommands
+{
+    class StopCommand
+    {
+        private static RoutedUICommand launch_command;
+        static StopCommand()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch",
+           typeof(StopCommand), myInputGestures);
+        }
+        public static RoutedUICommand Launch
+        {
+            get
+            {
+                return launch_command;
+            }
+        }
+    }
+}
